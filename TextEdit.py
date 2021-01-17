2b1703a00c7ea0ba4f7f540eeef0a64f44bc8fba
Axel Schneider <axel99092@t-online.de>
2021-01-17 15:05:55 +0100

    Update TextEdit.py
    added: drag/drop file

--- TextEdit.py
+++ TextEdit.py
@@ -14,8 +14,10 @@ gi.require_version('Keybinder', '3.0')
 from gi.repository import Gtk, Gdk, Keybinder, Pango
 import sys
 from os import path
+from urllib.request import url2pathname
 import warnings
 import undobuffer
+dnd_list = [Gtk.TargetEntry.new("text/uri-list", 0, 80)]
 
 warnings.filterwarnings("ignore")
 
@@ -43,6 +45,7 @@ class MyWindow(Gtk.Window):
         self.win = builder.get_object("window")
         
         self.q_icon = builder.get_object("question_icon")
+        self.md_icon = builder.get_object("question_icon")
         
         self.btn_save = builder.get_object("btn_save")
         self.btn_save.connect('clicked', self.save_file)
@@ -60,11 +63,16 @@ class MyWindow(Gtk.Window):
         self.btn_open.connect('clicked', self.on_open)
         self.btn_open.set_relief(Gtk.ReliefStyle.NONE)
         
+        ### textview
         self.editor = builder.get_object("editor")
         self.editor.set_accepts_tab(True)
         tabs = Pango.TabArray(1, True)
         tabs.set_tab(0, Pango.TabAlign.LEFT, 16)
         self.editor.set_tabs(tabs)
+
+        self.editor.drag_dest_set_target_list(dnd_list)
+
+        self.editor.connect("drag-data-received", self.on_drag_data_received)
         
         self.buffer = undobuffer.UndoableBuffer()
         self.buffer.connect('changed', self.is_modified)
@@ -101,11 +109,10 @@ class MyWindow(Gtk.Window):
             self.file_filter.add_pattern(p)
         
         self.win.connect("delete-event", self.on_close)
-        self.win.resize(800, 800)
+        self.win.resize(800, 700)
         self.win.move(0, 0)
         self.win.show_all()
         ### focus on editor
-        self.editor.grab_focus()
         self.findbox.set_visible(False)
         
         ### shortcuts
@@ -137,16 +144,37 @@ class MyWindow(Gtk.Window):
                 self.status_label.set_text(f"'{myfile}' loaded")
         else:
             self.status_label.set_text("Welcome to TextEdit")
+        self.editor.grab_focus()
+        self.is_changed = False
         Gtk.main() 
         
-    ### close window
-    def on_close(self, *args):
-        print(f"{self.current_file} changed: {self.is_changed}")
-        if self.is_changed:
-            if self.maybe_saved():
-                Gtk.main_quit()
-        else:
-            Gtk.main_quit()
+    ### drop file
+    def on_drag_data_received(self, widget, context, x, y, selection, target_type, timestamp):
+        myfile = ""
+        if target_type == 80:
+            uri = str(selection.get_data().decode().rstrip())
+            myfile = url2pathname(uri)[7:]
+            print(f'dropped file: {myfile}')
+            if self.is_changed:
+                self.maybe_saved()
+                self.open_file(myfile)
+            else:
+                self.open_file(myfile)
+                
+                
+    def open_file(self, myfile, *args):
+        with open(myfile, 'r') as f:
+            data = f.read()
+            self.buffer.set_text(data)
+            self.editor.set_buffer(self.buffer)
+            self.current_file = myfile
+            self.current_filename = myfile.rpartition("/")[2]
+            f.close()
+            self.headerbar.set_subtitle(myfile)
+            self.status_label.set_text(f"'{myfile}' loaded")
+            self.headerbar.set_title("TextEdit")
+            self.editor.grab_focus()
+            self.is_changed = False
         
     ### get editor text
     def get_buffer(self):
@@ -220,18 +248,63 @@ class MyWindow(Gtk.Window):
     ### set modified   
     def is_modified(self, *args):
         self.is_changed = True
+        self.headerbar.set_title("TextEdit*")
     
     ### new file clear editor
     def on_new_file(self, *args):
-        if not self.is_changed:
-            self.buffer = None
+        if not self.maybe_saved():
+            self.buffer.set_text("")
             self.editor.set_buffer(self.buffer)
             self.current_file = ""
+            self.headerbar.set_title("TextEdit") 
+            self.is_changed = False
         else:
-            if self.maybe_saved():
-                self.buffer = None
+            self.buffer.set_text("")
+            self.editor.set_buffer(self.buffer)
+            self.current_file = ""
+            self.headerbar.set_title("TextEdit")    
+            self.is_changed = False
+            
+    ### open file    
+    def on_open(self, *args):       
+        if not self.maybe_saved():
+            self.on_open_file()
+        else:
+            self.on_open_file()
+                
+    def on_open_file(self, *args):
+        data = ""
+        myfile = ""
+        dlg = Gtk.FileChooserDialog(title="Please choose a file", parent=None, action = 0)
+        dlg.add_buttons("Cancel", Gtk.ResponseType.CANCEL,
+             "Save", Gtk.ResponseType.OK)
+        dlg.add_filter(self.file_filter)
+
+        response = dlg.run()
+
+        if response == Gtk.ResponseType.OK:
+            myfile = dlg.get_filename()
+        else:
+            myfile = ("")
+
+        dlg.destroy()    
+        
+        
+        if not myfile == "":
+            self.buffer.set_text(data)
+            print("loading:", myfile)
+            with open(myfile, 'r') as f:
+                data = f.read()
+                self.buffer.set_text(data)
                 self.editor.set_buffer(self.buffer)
-                self.current_file = ""                   
+                self.current_file = myfile
+                self.current_filename = myfile.rpartition("/")[2]
+                f.close()
+                self.headerbar.set_subtitle(myfile)
+                self.status_label.set_text(f"'{myfile}' loaded")
+                self.headerbar.set_title("TextEdit")
+                self.editor.grab_focus()
+                self.is_changed = False
 
     ### file save as ...
     def on_save_file(self, *args):
@@ -266,7 +339,8 @@ class MyWindow(Gtk.Window):
                 self.status_label.set_text(f"'{myfile}' saved")
                 self.current_file = myfile
                 self.current_filename = myfile.rpartition("/")[2]
-                self.is_changed = False   
+                self.is_changed = False
+                self.headerbar.set_title("TextEdit")
                 
     ### save current file            
     def save_file(self, *args):
@@ -280,70 +354,46 @@ class MyWindow(Gtk.Window):
                 self.current_file = myfile
                 self.current_filename = myfile.rpartition("/")[2]
                 self.is_changed = False 
-                print("modified", self.is_changed)
+                self.headerbar.set_title("TextEdit")
         else:
             self.on_save_file()
+        return True
 
     ### ask to save changes
     def maybe_saved(self, *args):
-        md = Gtk.MessageDialog(image=self.q_icon, title="TextEdit", 
+        print("is modified", self.is_changed)
+        md = Gtk.MessageDialog(title="TextEdit", 
                                 text="The document was changed.\n\nSave changes?", 
-                                parent=None)
-        md.add_buttons("No", Gtk.ResponseType.CANCEL,
-             "Yes", Gtk.ResponseType.OK)
+                                parent=None, buttons=("Cancel", Gtk.ResponseType.CANCEL,
+             "Yes", Gtk.ResponseType.YES, "No", Gtk.ResponseType.NO))
+        md.set_image(self.q_icon)
         response = md.run()
-        
-        if response == Gtk.ResponseType.OK:
+        if response == Gtk.ResponseType.YES:
             ### save
-            if not self.current_file == "":
-                self.save_file(self.current_file)
-            else:
-                self.on_save_file()
-        else:
+            self.save_file()
             md.destroy()
+            return False
+        elif response == Gtk.ResponseType.NO:
+            md.destroy()
+            return False
+        elif response == Gtk.ResponseType.CANCEL:
+            md.destroy()
+            return True
         md.destroy()
-        return True
-            
-    ### open file    
-    def on_open(self, *args):       
+        
+    ### close window
+    def on_close(self, *args):
+        print(f"{self.current_file} changed: {self.is_changed}")
         if self.is_changed:
-            self.maybe_saved()
-            self.on_open_file()
+            b = self.maybe_saved()
+            print (f"close: {b}")
+            if b: 
+                return True
+            else:
+                Gtk.main_quit()
         else:
-            self.on_open_file()
-                
-    def on_open_file(self, *args):
-        data = ""
-        myfile = ""
-        dlg = Gtk.FileChooserDialog(title="Please choose a file", parent=None, action = 0)
-        dlg.add_buttons("Cancel", Gtk.ResponseType.CANCEL,
-             "Save", Gtk.ResponseType.OK)
-        dlg.add_filter(self.file_filter)
-
-        response = dlg.run()
-
-        if response == Gtk.ResponseType.OK:
-            myfile = dlg.get_filename()
-        else:
-            myfile = ("")
-
-        dlg.destroy()    
-        
-        
-        if not myfile == "":
-            self.buffer.set_text(data)
-            print("loading:", myfile)
-            with open(myfile, 'r') as f:
-                data = f.read()
-                self.buffer.set_text(data)
-                self.editor.set_buffer(self.buffer)
-                self.current_file = myfile
-                self.current_filename = myfile.rpartition("/")[2]
-                f.close()
-                self.is_changed = False
-                print("modified", self.is_changed)
-                self.headerbar.set_subtitle(myfile)
-                self.status_label.set_text(f"'{myfile}' loaded")
+            Gtk.main_quit()
+            
 
 if __name__ == "__main__":
     w = MyWindow()
